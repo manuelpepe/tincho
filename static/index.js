bfe3e8fea788000860ac52cbd896fc4e2443e1af
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2023-08-24 00:00:00 +0000

    (fix)card.value type

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    /** @typedef {{suit: string, value: string}} Card */
+    /** @typedef {{suit: string, value: number}} Card */
     /** @typedef {{id: string, points: number, pending_first_peek: boolean, cards_in_hand: number}} Player */
 
     const SUITS = {
