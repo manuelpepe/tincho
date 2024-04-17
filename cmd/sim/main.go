79b7427fa672e2e99a1e2e265a15a5c4f7f2f6f8
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2024-04-17 18:44:22 -0300

    (feat)flags for sim cli

--- cmd/sim/main.go
+++ cmd/sim/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"slices"
 	"time"
 
 	"github.com/manuelpepe/tincho/internal/bots"
@@ -39,25 +41,78 @@ func run(name string, iters int, showLogs bool, strats ...func() bots.Strategy) 
 	ctx := context.Background()
 	sum, err := sim.Compete(ctx, logger, iters, strats...)
 	if err != nil {
+		fmt.Printf("=== RUN: %s\n%s\n--- ERR (%s)\n\n", name, err, time.Since(start).String())
 		return err
 	}
 
-	end := time.Now().Sub(start)
-	fmt.Printf("=== RUN: %s\n%+v--- OK (%s)\n\n", name, sum.AsText(), end.String())
+	fmt.Printf("=== RUN: %s\n%+v--- OK (%s)\n\n", name, sum.AsText(), time.Since(start).String())
 	return nil
 }
 
 func main() {
-	go func() {
-		fmt.Println("Starting pprof")
-		fmt.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	var showLogs, all, eve, evm, evh, mvm, mvh, hvh, evmvh, pp bool
+	var iters int
 
-	run("EvE", 10000, false, easy, easy)
-	run("EvM", 10000, false, easy, medium)
-	run("EvH", 10000, false, easy, hard)
-	run("MvM", 10000, false, medium, medium)
-	run("MvH", 10000, false, medium, hard)
-	run("HvH", 10, false, hard, hard)
-	run("EvMvH", 10000, false, easy, medium, hard)
+	flag.BoolVar(&showLogs, "logs", false, "Show logs")
+	flag.BoolVar(&pp, "pp", false, "Run pprof")
+	flag.IntVar(&iters, "iters", 10000, "Number of iterations")
+
+	flag.BoolVar(&all, "all", false, "Run all")
+	flag.BoolVar(&eve, "ee", false, "Run Easy vs Easy")
+	flag.BoolVar(&evm, "em", false, "Run Easy vs Medium")
+	flag.BoolVar(&evh, "eh", false, "Run Easy vs Hard")
+	flag.BoolVar(&mvm, "mm", false, "Run Medium vs Medium")
+	flag.BoolVar(&mvh, "mh", false, "Run Medium vs Hard")
+	flag.BoolVar(&hvh, "hh", false, "Run Hard vs Hard")
+	flag.BoolVar(&evmvh, "emh", false, "Run Easy vs Medium vs Hard")
+
+	flag.Parse()
+
+	allFlags := []bool{eve, evm, evh, mvm, mvh, hvh, evmvh}
+	if all && slices.Contains(allFlags, true) {
+		fmt.Println("Cannot use -all with other sim specific flags")
+		return
+	}
+
+	if pp {
+		go func() {
+			fmt.Println("Starting pprof")
+			fmt.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
+
+	if all || eve {
+		run("EvE", iters, showLogs, easy, easy)
+	}
+
+	if all || evm {
+		run("EvM", iters, showLogs, easy, medium)
+	}
+
+	if all || evh {
+		run("EvH", iters, showLogs, easy, hard)
+	}
+
+	if all || mvm {
+		run("MvM", iters, showLogs, medium, medium)
+	}
+
+	if all || mvh {
+		run("MvH", iters, showLogs, medium, hard)
+	}
+
+	if all || hvh {
+		oldIters := iters
+		if all && iters > 10 {
+			fmt.Println("[I] Reducing iterations to 10 for HvH")
+			fmt.Println("    (HvH simulations can take a long time going back and forth in points)")
+			iters = 10
+		}
+		run("HvH", oldIters, showLogs, hard, hard)
+		iters = oldIters
+	}
+
+	if all || evmvh {
+		run("EvMvH", iters, showLogs, easy, medium, hard)
+	}
 }
